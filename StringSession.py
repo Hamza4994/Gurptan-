97eebb3a58f5db95081045b4eb7614648a6a6a00
berce <75228263+must4f@users.noreply.github.com>
2022-04-25 08:02:53 +0300

    Update StringSession.py

--- StringSession.py
+++ StringSession.py
@@ -5,7 +5,7 @@ API_ID   = input("Telegram API ID: ")
 API_HASH = input("Telegram API HASH: ")
 
 async def session_olustur(api_id, api_hash):
-    async with Client(":memory:", api_id=api_id, api_hash=api_hash) as app:
+    async with Client("userbot", api_id=api_id, api_hash=api_hash) as app:
         print('\n\n')
         print(await app.export_session_string())
         print('\n')
